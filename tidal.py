87296070966cabf76111b4b943a755c929e60a78
loboda4450 <loboda4450@gmail.com>
2019-08-08 20:13:40 +0200

    Improvements in queue, track and album downloads.

--- tidal.py
+++ tidal.py
@@ -16,14 +16,16 @@ import queue
 import threading
 import sys
 import time
+import re
 from colorama import init, Fore, Back
+import os
 
 
 class QueueObject:
     def __init__(self):
         pass
 
-    def download(self):
+    def download(self, boolean):
         pass
 
     def display(self):
@@ -35,75 +37,122 @@ class QueueTrack(QueueObject):
         self.track = track
         self.folder = folder
 
+    def download(self, boolean):
+        try:
+            for dir in os.listdir(self.folder):
+                if dir == self.track.artist.name.replace("/", "_"):
+                    folder = dir
+                    break
+                else:
+                    folder = self.folder / '1. No album'
 
-    def download(self):
-        track_name = f'{self.track.name}{f" ({self.track.version})" if self.track.version else ""}'
-        print(Fore.GREEN + f'\nDownloading track: {self.track.artist.name} - {track_name}')
-        download_flac(self.track, self.folder / f'{self.track.artist.name} - {track_name}.flac'.replace("/", "_"))
-        print(Fore.CYAN + f'\nTrack: {self.track.artist.name} - {track_name} downloaded!')
+            track_name = f'{self.track.name}{f" ({self.track.version})" if self.track.version else ""}'
+            track_name = re.sub('\W+', ' ', track_name)
+            if boolean:
+                print(Fore.GREEN + f'\nDownloading track: {self.track.artist.name} - {track_name} to the {folder}')
+            download_flac(self.track, folder / f'{self.track.artist.name} - {track_name}.flac'.replace("/", "_"))
+            print(Fore.CYAN + f'\nTrack: {self.track.artist.name} - {track_name} downloaded!')
+        except FLACNoHeaderError:
+            print(Fore.BLACK + Back.RED + "This track is not available in lossless quality, abandoning")
+        except ConnectionError:
+            print(Fore.BLACK + Back.RED + "Failed to connect, abandoning")
+        except PermissionError:
+            print(Fore.BLACK + Back.RED + "Check if defined folder is not being used by other app, abandoning")
+        except requests.exceptions.HTTPError as e:
+            if str(e).split(' ')[0] == '401':
+                print(Fore.BLACK + Back.RED + "This track is not available in your country, abandoning")
 
     def display(self):
         track_name = f'{self.track.name}{f" ({self.track.version})" if self.track.version else ""}'
-        print(f'Track {self.track.artist.name} - {track_name}')
+        print(Fore.LIGHTMAGENTA_EX + f'Track "{track_name}" created by {self.track.artist.name}')
 
 
 class QueueAlbum(QueueObject):
-    def __init__(self, album, album_id, folder):
+    def __init__(self, album, folder):
         self.album = album
-        self.album_id = album_id
         self.folder = folder
 
-    def download(self):
-        print(Fore.GREEN + f'Downloading album: {self.album.artist.name} - {self.album.name}')
-        tracks = session.get_album_tracks(album_id=self.album_id)  # type: typing.Iterable[tidalapi.models.Track]
-        num = 0
-        # TODO: handle multicd albums better (separate dirs and playlists?)
-        discs = max(map(lambda x: x.disc_num, tracks))
-        folder = self.folder / self.album.artist.name.replace("/", "_") / f'{f"({self.album.release_date.year}) " if self.album.release_date is not None else ""}{self.album.name}'.replace("/", "_")
-        folder.mkdir(parents=True, exist_ok=True)
-        with open(folder / f'00. {self.album.name.replace("/", "_")}.m3u', 'w') as playlist_file:
-            for track in tracks:
-                num += 1
-                track_name = f'{track.name}{f" ({track.version})" if track.version else ""}'
-                track_name = track_name.replace('"', '')
-                print(Fore.GREEN + f'Downloading ({num}/{self.album.num_tracks}): {track_name}')  # printing each downloaded element kills clarity so badly//not if coloured
-                fname = f'{str(track.track_num).zfill(2)}. {track_name.replace("/", "_")}.flac'
-                download_flac(track, folder / fname, album=self.album)
-                playlist_file.write(fname)
-                playlist_file.write("\n")
-        print(Fore.CYAN + f'\nAlbum {self.album.artist.name} - {self.album.name} downloaded!' + '\n')
+    def download(self, boolean):
+        try:
+            print(Fore.GREEN + f'Downloading album: {self.album.artist.name} - {self.album.name}')
+            tracks = session.get_album_tracks(album_id=self.album.id)  # type: typing.Iterable[tidalapi.models.Track]
+            num = 0
+            # TODO: handle multicd albums better (separate dirs and playlists?)
+            discs = max(map(lambda x: x.disc_num, tracks))
+            folder = self.folder / self.album.artist.name.replace("/",
+                                                                  "_") / f'{f"({self.album.release_date.year}) " if self.album.release_date is not None else ""}{self.album.name}'.replace(
+                "/", "_")
+            folder.mkdir(parents=True, exist_ok=True)
+            with open(folder / f'00. {self.album.name.replace("/", "_")}.m3u', 'w') as playlist_file:
+                for track in tracks:
+                    num += 1
+                    try:
+                        track_name = f'{track.name}{f" ({track.version})" if track.version else ""}'
+                        track_name = re.sub('\W+', ' ', track_name)
+                        if boolean:
+                            print(
+                                Fore.GREEN + f'Downloading ({num}/{self.album.num_tracks}): {track_name}')  # printing
+                            # each downloaded element kills clarity so badly//not if coloured
+                        fname = f'{str(track.track_num).zfill(2)}. {track_name.replace("/", "_")}.flac'
+                        download_flac(track, folder / fname, album=self.album)
+                        playlist_file.write(fname)
+                        playlist_file.write("\n")
+                    except requests.exceptions.HTTPError as e:
+                        if str(e).split(' ')[0] == '401':
+                            print(Fore.BLACK + Back.RED + "This track is not available in your country")
+
+            print(Fore.CYAN + f'\nAlbum {self.album.artist.name} - {self.album.name} downloaded!' + '\n')
+        except FLACNoHeaderError:
+            print(Fore.BLACK + Back.RED + "This album is not available in lossless quality, abandoning")
+        except ConnectionError:
+            print(Fore.BLACK + Back.RED + "Failed to connect, abandoning")
+        except PermissionError:
+            print(Fore.BLACK + Back.RED + "Check if defined folder is not being used by other app, abandoning")
+        except requests.exceptions.HTTPError as e:
+            if str(e).split(' ')[0] == '401':
+                print(Fore.BLACK + Back.RED + "This album is not available in your country, abandoning")
 
     def display(self):
-        print(Fore.LIGHTMAGENTA_EX + f'Album {self.album.artist.name} - {self.album.name}')
+        print(Fore.LIGHTMAGENTA_EX + f'Album "{self.album.name}" created by {self.album.artist.name}')
         pass
 
 
 class QueuePlaylist(QueueObject):
-    def __init__(self, playlist, playlist_id, folder):
+    def __init__(self, playlist, folder):
         self.playlist = playlist
-        self.playlist_id = playlist_id
         self.folder = folder
 
-    def download(self):
-        print(Fore.GREEN + f'Downloading playlist: {self.playlist.name}')
-        tracks = session.get_playlist_tracks(playlist_id=playlist_id)
-        num = 0
-        folder = self.folder / self.playlist.name.replace("/", "_")
-        folder.mkdir(parents=True, exist_ok=True)
-        with open(folder / f'{self.playlist.name.replace("/", "_")}.m3u', "w") as playlist_file:
-            for track in tracks:
-                num += 1
-                track_name = f'{track.name}{f" ({track.version})" if track.version else ""}'
-                track_name = track_name.replace('"', '')
-                print(f'Downloading ({num}/{self.playlist.num_tracks}): {track_name}')
-                fname = f'{track.artist.name} - {track_name}.flac'.replace("/", "_")
-                download_flac(track, folder / fname)
-                playlist_file.write(fname)
-                playlist_file.write("\n")
-        print(Fore.CYAN + f'\nPlaylist {self.playlist.name} downloaded!' + '\n')
+    def download(self, boolean):
+        try:
+            print(Fore.GREEN + f'Downloading playlist: {self.playlist.name}')
+            tracks = session.get_playlist_tracks(playlist_id=self.playlist.id)
+            num = 0
+            folder = self.folder / self.playlist.name.replace("/", "_")
+            folder.mkdir(parents=True, exist_ok=True)
+            with open(folder / f'{self.playlist.name.replace("/", "_")}.m3u', "w") as playlist_file:
+                for track in tracks:
+                    num += 1
+                    track_name = f'{track.name}{f" ({track.version})" if track.version else ""}'
+                    track_name = re.sub('\W+', ' ', track_name)
+                    if boolean:
+                        print(Fore.GREEN + f'Downloading ({num}/{self.playlist.num_tracks}): {track_name}')
+                    fname = f'{track.artist.name} - {track_name}.flac'.replace("/", "_")
+                    download_flac(track, folder / fname)
+                    playlist_file.write(fname)
+                    playlist_file.write("\n")
+            print(Fore.CYAN + f'\nPlaylist {self.playlist.name} downloaded!' + '\n')
+        except FLACNoHeaderError:
+            print(Fore.BLACK + Back.RED + "This playlist is not available in lossless quality, abandoning")
+        except ConnectionError:
+            print(Fore.BLACK + Back.RED + "Failed to connect, abandoning")
+        except PermissionError:
+            print(Fore.BLACK + Back.RED + "Check if defined folder is not being used by other app, abandoning")
+        except requests.exceptions.HTTPError as e:
+            if e == '401':
+                print(Fore.BLACK + Back.RED + "This playlist is not available in your country, abandoning")
 
     def display(self):
-        print(self.playlist.name)
+        print(Fore.LIGHTMAGENTA_EX + f'Playlist "{self.playlist.name}" created by {self.playlist.creator}')
 
 
 def get_track_title(track):
@@ -179,21 +228,35 @@ def download_flac(track: tidalapi.models.Track, file_path, album=None):
         shutil.copyfileobj(data, f)
 
 
-def download_thread(q):
+def download_thread(q, boolean):
     while True:
         if q.qsize() > 0:
             tmp = q.get()
-            tmp.download()
+            print(Fore.LIGHTMAGENTA_EX + q.qsize() + 'elements left in queue')
+            tmp.download(boolean)
         time.sleep(1)
+
+
+def internet_access():
+    try:
+        requests.get("http://google.com", timeout=3)
+        return True
+    except requests.ConnectionError:
+        pass
+
+    return False
+
 
 if __name__ == "__main__":
     import argparse
+
     init(autoreset=True)
 
-    q = queue.Queue()  # infinite queue
-    d_thread = threading.Thread(target=download_thread, args=(q,))
-    d_thread.start()  # starting a download thread
-    print("Download thread has started\n")
+    if not internet_access():
+        print(Fore.LIGHTRED_EX + Back.BLACK + "You have no internet connection, exiting")
+        sys.exit()
+    else:
+        print(Fore.LIGHTGREEN_EX + Back.BLACK + "Internet connection checked!")
 
     p = argparse.ArgumentParser()
     p.add_argument('login', help="TIDAL login/email")
@@ -207,15 +270,27 @@ if __name__ == "__main__":
     session = tidalapi.Session(config)
     session.login(args.login, args.password)
 
+    print(Fore.LIGHTRED_EX + "Do you want to display downloaded elements? [y/n]")
+    if input() != 'n':
+        boolean = True
+    else:
+        boolean = False
+
+    q = queue.Queue()  # infinite queue
+    d_thread = threading.Thread(target=download_thread, args=(q, boolean))
+    d_thread.start()  # starting a download thread
+    print(Fore.LIGHTYELLOW_EX + "Download thread has started\n")
+
     print(Back.BLUE + Fore.LIGHTGREEN_EX + "Tidal FLAC ripper\n")
 
     while True:
-        folder = Path(args.output_dir)
+        folder = Path('H:/Flaczki')
         folder.mkdir(parents=True, exist_ok=True)
 
         print(
-            "0) Search for track\n1) Download track\n2) Download album\n3) Download playlist\n4) Display queue\n5) Exit\n")
-        mode = input("Select mode:")
+            "0) Search for track\n1) Download track\n2) Download album\n3) Download playlist\n4) Display queue\n5) "
+            "Exit\n")
+        mode = input("Select mode:\n")
 
         # TODO: download queue
         # TODO: search for album
@@ -230,26 +305,25 @@ if __name__ == "__main__":
                     print(f"{track.id}: {track.artist.name} - {track.name}")
 
             elif mode == "1":
-                link = input("Enter link or track id: ")
+                link = input("Enter link or track id: \n")
                 track_id = link.split('/')[-1]
                 track = session.get_track(track_id, withAlbum=True)
                 q.put(QueueTrack(track, folder))  # adding a track to download queue
                 print(Fore.RED + f'\nEnqueued track: {track.artist.name} - {track.name}' + '\n')
 
-
             elif mode == "2":
-                link = input("Enter album link or id: ")
+                link = input("Enter album link or id: \n")
                 album_id = link.split('/')[-1]
                 album = session.get_album(album_id=album_id)
-                q.put(QueueAlbum(album, album_id, folder))  # adding a track to download queue
+                q.put(QueueAlbum(album, folder))  # adding a track to download queue
                 print(Fore.RED + f'\nEnqueued album: {album.artist.name} - {album.name}' + '\n')
 
             elif mode == "3":
-                link = input("Enter playlist link or id: ")
+                link = input("Enter playlist link or id: \n")
                 playlist_id = link.split('/')[-1]
                 playlist = session.get_playlist(playlist_id=playlist_id)
-                q.put(QueuePlaylist(playlist, playlist_id, folder))
-                print(Fore.RED + f'\nEnqueued playlist: {playlist.name} + \n')
+                q.put(QueuePlaylist(playlist, folder))
+                print(Fore.RED + f'\nEnqueued playlist: {playlist.name} \n')
 
             elif mode == "4":
                 if not q.empty():
@@ -260,16 +334,15 @@ if __name__ == "__main__":
                 else:
                     print(Fore.LIGHTMAGENTA_EX + "Queue is empty\n")
 
-
             elif mode == "5":
                 if not q.empty():
                     sys.exit("Queue is not empty, ripper will shut down immediately after the download process")
                 else:
-                    sys.exit("Thanks for using tidal ripper. Don't close this window if your last download hasn't finished. See you around!")
+                    sys.exit(
+                        "Thanks for using tidal ripper. Don't close this window if your last download hasn't "
+                        "finished. See you around!")
             else:
                 print("Incorrect mode!")
 
-        except FLACNoHeaderError:
-            print(Fore.BLACK + Back.RED + "This track is not available in lossless quality, abandoning")
         except Exception as e:
             print(f"Error occurred: {e}")
